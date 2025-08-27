222aa8be8198959677fcc011b97c553d4013d43d
William Schultz <william.schultz@mongodb.com>
2025-08-26 21:29:45 -0400

    Bit more comfort

--- js/app.js
+++ js/app.js
@@ -2526,7 +2526,7 @@ function midPane() {
     return [
         m("div", { 
             id: "mid-pane", 
-            style: {width: model.tracePaneHidden ? "90%" : "40%"} 
+            style: {width: model.tracePaneHidden ? "90%" : "45%"} 
         }, tabs)
     ];
 }
@@ -2630,7 +2630,7 @@ function tracePane() {
 
     return m("div", { 
             id: "trace-container", 
-            style: {width: model.tracePaneHidden ? "5%" : "60%"}
+            style: {width: model.tracePaneHidden ? "5%" : "55%"}
         }, [
         tabs,
         otherTabs
